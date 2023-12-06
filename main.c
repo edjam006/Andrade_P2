9b702f7cc7e50733a4e9d2fd5bb03fd50edded21
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2023-12-06 15:24:16 -0500

    Variables Declaradas junto operacion dentro de la matriz para recorrer cada valor y calcular el promedio de alumnos y Progresos

--- main.c
+++ main.c
@@ -8,3 +8,35 @@
 float generarCalificaciones() {
     return (float)(rand() % 11);  // Genera un número entre 0 y 10
 }
+
+float Promedio(float calificaciones[], int total) {
+    float suma = 0.0;
+    for (int i = 0; i < total; i++) {
+        suma += calificaciones[i];
+    }
+    return suma / total;
+}
+
+int main() {
+    float calificaciones[ALUMNOS][PROGRESOS];
+    float promediosA[ALUMNOS] = {0.0};
+    float promediosP[PROGRESOS] = {0.0};
+    int mayorAlumno = 0;
+    float mayorPromedio = 0.0;
+
+    srand(time(NULL));  
+
+    for (int i = 0; i < ALUMNOS; i++) {
+        for (int j = 0; j < PROGRESOS; j++) {
+            calificaciones[i][j] = generarCalificaciones();
+            promediosP[j] += calificaciones[i][j];
+            promediosA[i] = calcularPromedio(calificaciones[i], PROGRESOS);
+        if (promediosA[i] > mayorPromedio) {
+            mayorAlumno = promediosA[i];
+            mayorPromedio = i;
+        }
+        }
+        
+    }
+return 0;
+} 
